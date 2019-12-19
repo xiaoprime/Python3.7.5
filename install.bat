42c4b8b1f97e811ea702c6bbb0e02d80d0fafd1c
Prime <31033455+xiaoprime@users.noreply.github.com>
2019-12-19 14:44:47 +0800

    update install.bat

---
+++ install.bat
@@ -0,0 +1,112 @@
+cd pakages
+pip install numpy-1.17.4-cp37-cp37m-win_amd64.whl
+pip install scipy-1.3.3-cp37-cp37m-win_amd64.whl
+pip install six-1.13.0-py2.py3-none-any.whl
+pip install cycler-0.10.0-py2.py3-none-any.whl
+pip install kiwisolver-1.1.0-cp37-none-win_amd64.whl
+pip install python_dateutil-2.8.0-py2.py3-none-any.whl
+pip install pyparsing-2.4.5-py2.py3-none-any.whl
+pip install matplotlib-3.1.2-cp37-cp37m-win_amd64.whl
+
+pip install soupsieve-1.9.5-py2.py3-none-any.whl
+pip install beautifulsoup4-4.8.1-py3-none-any.whl
+pip install pytz-2019.3-py2.py3-none-any.whl
+pip install pandas-0.25.3-cp37-cp37m-win_amd64.whl
+pip install urllib3-1.25.7-py2.py3-none-any.whl
+pip install selenium-3.141.0-py2.py3-none-any.whl
+pip install cymem-2.0.3-cp37-cp37m-win_amd64.whl
+pip install murmurhash-1.0.2-cp37-cp37m-win_amd64.whl
+pip install preshed-3.0.2-cp37-cp37m-win_amd64.whl
+pip install wasabi-0.4.2-py3-none-any.whl
+pip install more_itertools-8.0.0-py3-none-any.whl
+pip install zipp-0.6.0-py2.py3-none-any.whl
+pip install importlib_metadata-1.1.0-py2.py3-none-any.whl
+pip install catalogue-0.0.8-py2.py3-none-any.whl
+pip install plac-1.1.3-py2.py3-none-any.whl
+pip install tqdm-4.40.0-py2.py3-none-any.whl
+pip install srsly-0.2.0-cp37-cp37m-win_amd64.whl
+pip install blis-0.4.1-cp37-cp37m-win_amd64.whl
+pip install thinc-7.3.1-cp37-cp37m-win_amd64.whl
+pip install certifi-2019.11.28-py2.py3-none-any.whl
+pip install chardet-3.0.4-py2.py3-none-any.whl
+pip install idna-2.8-py2.py3-none-any.whl
+pip install requests-2.22.0-py2.py3-none-any.whl
+pip install spacy-2.2.3-cp37-cp37m-win_amd64.whl
+pip install vaderSentiment-3.2.1-py2.py3-none-any.whl
+pip install nltk-3.4.5.zip
+pip install textblob-0.15.3-py2.py3-none-any.whl
+pip install chromedriver-binary-74.0.3729.6.0.tar.gz
+pip install tweety-0.1.6.tar.gz
+
+pip install dialite-0.5.2.tar.gz
+pip install webruntime-0.5.6.tar.gz
+pip install pscript-0.7.1-py3-none-any.whl
+pip install lxml-4.4.2-cp37-cp37m-win_amd64.whl
+pip install pandas_datareader-0.8.1-py2.py3-none-any.whl
+pip install multitasking-0.0.9.tar.gz
+pip install yfinance-0.1.50.tar.gz
+pip install decorator-4.4.1-py2.py3-none-any.whl
+pip install networkx-2.2.zip
+pip install cloudpickle-1.2.2-py2.py3-none-any.whl
+pip install future-0.18.2.tar.gz
+pip install hyperopt-0.2.2-py3-none-any.whl
+pip install tornado-5.0.tar.gz
+
+pip install protobuf-3.11.0-cp37-cp37m-win_amd64.whl
+pip install astor-0.8.0-py2.py3-none-any.whl
+pip install termcolor-1.1.0.tar.gz
+pip install oauthlib-3.1.0-py2.py3-none-any.whl
+pip install requests_oauthlib-1.3.0-py2.py3-none-any.whl
+pip install cachetools-3.1.1-py2.py3-none-any.whl
+pip install pyasn1-0.4.8-py2.py3-none-any.whl
+pip install pyasn1_modules-0.2.7-py2.py3-none-any.whl
+pip install rsa-4.0-py2.py3-none-any.whl
+pip install google_auth-1.7.1-py2.py3-none-any.whl
+pip install google_auth_oauthlib-0.4.1-py2.py3-none-any.whl
+pip install Werkzeug-0.16.0-py2.py3-none-any.whl
+pip install Markdown-3.1.1-py2.py3-none-any.whl
+pip install grpcio-1.25.0-cp37-cp37m-win_amd64.whl
+pip install absl-py-0.8.1.tar.gz
+pip install wheel-0.33.6-py2.py3-none-any.whl
+pip install tensorboard-2.0.2-py3-none-any.whl
+pip install gast-0.2.2.tar.gz
+pip install google_pasta-0.1.8-py3-none-any.whl
+pip install Keras_Preprocessing-1.1.0-py2.py3-none-any.whl
+pip install h5py-2.10.0-cp37-cp37m-win_amd64.whl
+pip install Keras_Applications-1.0.8-py3-none-any.whl
+pip install wrapt-1.11.2.tar.gz
+pip install tensorflow_estimator-2.0.1-py2.py3-none-any.whl
+pip install opt_einsum-3.1.0.tar.gz
+pip install tensorflow-2.0.0-cp37-cp37m-win_amd64.whl
+
+pip install joblib-0.14.0-py2.py3-none-any.whl
+pip install scikit_learn-0.21.3-cp37-cp37m-win_amd64.whl
+pip install sklearn-0.0.tar.gz
+
+pip install jmespath-0.9.4-py2.py3-none-any.whl
+pip install docutils-0.15.2-py3-none-any.whl
+pip install botocore-1.13.30-py2.py3-none-any.whl
+pip install s3transfer-0.2.1-py2.py3-none-any.whl
+pip install boto3-1.10.30-py2.py3-none-any.whl
+
+pip install py-1.8.0-py2.py3-none-any.whl
+pip install attrs-19.3.0-py2.py3-none-any.whl
+pip install wcwidth-0.1.7-py2.py3-none-any.whl
+pip install packaging-19.2-py2.py3-none-any.whl
+pip install colorama-0.4.1-py2.py3-none-any.whl
+pip install atomicwrites-1.3.0-py2.py3-none-any.whl
+pip install pluggy-0.13.1-py2.py3-none-any.whl
+pip install pytest-5.3.1-py3-none-any.whl
+
+pip install Pillow-6.2.1-cp37-cp37m-win_amd64.whl
+pip install XlsxWriter-1.2.6-py2.py3-none-any.whl
+pip install python-pptx-0.6.18.tar.gz
+pip install xlrd-1.2.0-py2.py3-none-any.whl
+
+pip install PySocks-1.7.1-py3-none-any.whl
+pip install tweepy-3.8.0-py2.py3-none-any.whl
+pip install mpl_finance-0.10.0.tar.gz
+
+pip install jdcal-1.4.1-py2.py3-none-any.whl
+pip install et_xmlfile-1.0.1.tar.gz
+pip install openpyxl-3.0.2.tar.gz
